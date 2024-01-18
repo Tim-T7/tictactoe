9d81a270059b91eb46eec60215690caecdb6ca07
Timmy Tran <timmy.tran@mrjaffesclass.com>
2024-01-18 10:36:04 -0800

    Initial commit

---
+++ tictactoe/UI.java
@@ -0,0 +1,15 @@
+package tictactoe;
+import java.util.Scanner;
+
+/**
+ * UI class
+ */
+public class UI
+{
+
+    Scanner scanner;
+
+    public UI() {
+        scanner = new Scanner(System.in);         
+    }
+}
