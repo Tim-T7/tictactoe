8c2f3655157cc8087c36e785b6e5dab01774dc0a
Timmy Tran <timmy.tran@mrjaffesclass.com>
2024-01-23 11:39:45 -0800

    Commit message

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -13,11 +13,29 @@ public class State
 
     public boolean isWinner() {
         // You will write this code!!
-        
+        int total; 
+        for (int row=0; row<Constants.BOARD_SIZE; row++) {
+            total = getBoardCell(row, 0) +
+            getBoardCell (row, 1) +
+            getBoardCell (row,2);
+            if (total ==-3||total == 3) return true;
+        }
+        for (int col =0; col<Constants.BOARD_SIZE; col++) {
+            total = getBoardCell (0,col) + 
+            getBoardCell (1,col) +
+            getBoardCell (2,col);
+            if (total==-3||total ==3) return true;
+        }
+        total = getBoardCell (0,0) + getBoardCell (1,1) + getBoardCell(2,2);
+        if (total == -3 || total ==3) return true;
+        total = getBoardCell (2,0) + getBoardCell(1,1) + getBoardCell (0,2);
+        if (total == -3 || total ==3) return true;
+        return false;
     }
 
     public boolean isTie() {
         // You will write this code too!!
+        
     }
 
     public int getBoardCell(int row, int col) {
